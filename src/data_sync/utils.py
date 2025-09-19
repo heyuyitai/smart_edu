29b73d813098456d7720d05fdf1aad5c17c8112f
heyuyitai <heyuyitai@gmail.com>
2025-09-19 10:39:21 +0800

    提交MysqlReader

---
+++ src/data_sync/utils.py
@@ -0,0 +1,23 @@
+import pymysql
+from pymysql.cursors import DictCursor
+
+from src.config import config
+
+
+class MySqlReader:
+    def __init__(self):
+        self.conn=pymysql.connect(**config.MYSQL_CONFIG)
+        self.cursor=self.conn.cursor(DictCursor)
+
+    def read(self):
+        sql="""
+        select * from favor_info
+        """
+        self.cursor.execute(sql)
+        res=self.cursor.fetchall()
+        print(res)
+
+
+if __name__=="__main__":
+    sql_reader=MySqlReader()
+    sql_reader.read()
